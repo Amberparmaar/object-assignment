c9d5e46bbef20179a0a62c2b126d90b5d8c97378
AmberParmaar <160378243+Amberparmaar@users.noreply.github.com>
2024-03-20 02:59:08 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,35 @@
+//object , arrays, arrays on object and nested object
+let user = {
+    firstName : "Faizan",
+    lastName : "Mehmood Ayaz",
+    DateOfBirth : "18th june , 1995",
+    age : 31,
+    isHemale : true,
+    status : "Married",
+    country : "Pakistan",
+    City : "Karachi",
+    address : {
+        street : "3D block drigh road",
+        floor : "5th",
+        area : "Jamshed quater",
+    },
+    phoneNumber :{
+        Home : "021-2345678",
+        mobile : "03128675473",
+        office : "021-2345890",
+    },
+    email : "Example23@gmail.com",
+    salary : 30000,
+    AboutHim : {
+        Hobbies : ["cricket", "Body Building", "eating in restraunts"],
+        favoriteColour : "Black",
+        Entertainment : ["Watching movies", "outing with friends", ]
+    }
+    
+    
+ }
+ console.log(user.AboutHim.Hobbies[2]);
+ console.log(user.address.floor , user.address.area);
+ console.log('His monthly salary is ' + 'amount = ' + 30000);
+ console.log(user);
+ console.log(user.DateOfBirth);
